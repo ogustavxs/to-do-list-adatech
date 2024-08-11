22c55f5e01974b40be2de1a2573ee78289c1711e
Gustavo Pessoa <113478928+ogustavxs@users.noreply.github.com>
2024-08-11 14:39:09 -0300

    sistema integrado com arrays

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const form = document.querySelector("#todo-form");
 const titleInput = document.querySelector("#tasktitle");
 const toDoList = document.querySelector("#todo-list");
 
+let tasks = [];
 
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -16,6 +17,11 @@ form.addEventListener("submit", (e) => {
 	const spanText = document.createElement("span");
 	spanText.textContent = title;
 
+    tasks.push({
+        title: title,
+        done: false
+    })
+
 	const itemCheckbox = document.createElement("input");
 	itemCheckbox.setAttribute("type", "checkbox");
 	itemCheckbox.addEventListener("click", (e) => {
@@ -25,12 +31,28 @@ form.addEventListener("submit", (e) => {
 		} else {
 			spannode.style.textDecoration = "none";
 		}
+
+        tasks = tasks.map(t => {
+            if (t.title === spannode.textContent) {
+                return {
+                    title: t.title,
+                    done: !t.done
+                }
+            }
+
+            return t
+        })
+        console.log(tasks)
 	});
+
 
 	const removeButton = document.createElement("button");
 	removeButton.textContent = "Remover";
 	removeButton.addEventListener("click", (e) => {
 		toDoList.removeChild(e.target.parentElement);
+        tasks = tasks.filter((i) => {
+            return i.title !== e.target.parentElement.querySelector("span").textContent
+        })
 	});
 
 	item.appendChild(itemCheckbox);
