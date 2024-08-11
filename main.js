a200c3249f025ca3ffa5bcaf00d5786e26a482b4
Gustavo Pessoa <113478928+ogustavxs@users.noreply.github.com>
2024-08-11 15:03:30 -0300

    sistema integrado com o local server

--- main.js
+++ main.js
@@ -4,23 +4,15 @@ const toDoList = document.querySelector("#todo-list");
 
 let tasks = [];
 
-form.addEventListener("submit", (e) => {
-	e.preventDefault();
-
-	const title = titleInput.value;
-	titleInput.value = "";
-
-	if (title.length < 3) return;
-
+function addItemToList(title, done = false) {
 	const item = document.createElement("li");
 
 	const spanText = document.createElement("span");
 	spanText.textContent = title;
 
-    tasks.push({
-        title: title,
-        done: false
-    })
+	if (done) {
+		spanText.style.textDecoration = "line-through";
+	}
 
 	const itemCheckbox = document.createElement("input");
 	itemCheckbox.setAttribute("type", "checkbox");
@@ -32,31 +24,61 @@ form.addEventListener("submit", (e) => {
 			spannode.style.textDecoration = "none";
 		}
 
-        tasks = tasks.map(t => {
-            if (t.title === spannode.textContent) {
-                return {
-                    title: t.title,
-                    done: !t.done
-                }
-            }
+		tasks = tasks.map((t) => {
+			if (t.title === spannode.textContent) {
+				return {
+					title: t.title,
+					done: !t.done,
+				};
+			}
 
-            return t
-        })
-        console.log(tasks)
+			return t;
+		});
+		localStorage.setItem("tasks", JSON.stringify(tasks));
 	});
-
+	itemCheckbox.checked = done;
 
 	const removeButton = document.createElement("button");
 	removeButton.textContent = "Remover";
 	removeButton.addEventListener("click", (e) => {
 		toDoList.removeChild(e.target.parentElement);
-        tasks = tasks.filter((i) => {
-            return i.title !== e.target.parentElement.querySelector("span").textContent
-        })
+		tasks = tasks.filter((i) => {
+			return (
+				i.title !== e.target.parentElement.querySelector("span").textContent
+			);
+		});
+		localStorage.setItem("tasks", JSON.stringify(tasks));
 	});
 
 	item.appendChild(itemCheckbox);
 	item.appendChild(spanText);
 	item.appendChild(removeButton);
 	toDoList.appendChild(item);
+}
+
+window.onload = () => {
+	const tasksOnLocalStorage = localStorage.getItem("tasks");
+	if (!tasksOnLocalStorage) return;
+	tasks = JSON.parse(tasksOnLocalStorage);
+	// biome-ignore lint/complexity/noForEach: <explanation>
+	tasks.forEach((t) => {
+		addItemToList(t.title, t.done);
+	});
+};
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	const title = titleInput.value;
+	titleInput.value = "";
+
+	if (title.length < 3) return;
+
+	tasks.push({
+		title: title,
+		done: false,
+	});
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+
+	addItemToList(title);
 });
